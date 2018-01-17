f89bb784237549127d43f5066c50f5539e910caa
Devlon Duthie <duthied@gmail.com>
2018-01-17 14:23:24 -0500

    Adds refresh functionality

--- src/Alerts.js
+++ src/Alerts.js
@@ -5,13 +5,20 @@ import './App.css';
 import moment from 'moment';
 
 function AlertHeader(props) {
-
   return (
     <div className='alert-header' style={{'fontWeight': 'bold'}}>
-      { props.alertCount } Alerts <span className='updatedAt'>{ props.updatedAt }</span>
+
+      <button id='update' onClick={props.handler}>
+          update
+        </button>
+
+      { props.alertCount } Alerts 
+      <div className='updatedAt'>
+        { props.updatedAt }
+      </div>
+
     </div>
   )
-
 }
 
 function ItemHeader(props) {
@@ -80,6 +87,8 @@ class Alerts extends Component {
 
     // bind addtional functions
     this.timer = this.timer.bind(this);
+    this.update = this.update.bind(this);
+    this.fetchRSS = this.fetchRSS.bind(this);
   }
   
   componentDidMount() {
@@ -87,6 +96,30 @@ class Alerts extends Component {
     // store intervalId in the state so it can be accessed later:
     this.setState({intervalId: intervalId});
 
+    this.fetchRSS(this.state.rssFeed);
+
+    // var parser = require('rss-parser');
+    // let self = this;
+
+    // parser.parseURL(this.state.rssFeed, function(err, parsed) {
+    //   if (err) {
+    //     return;
+    //   }
+
+    //   let alerts = []; 
+    //   let count = 0;
+    //   parsed.feed.entries.forEach( function(alert) { 
+    //     alerts[count] = alert.date + ' - ' + alert.content;
+    //     count++;
+    //   });
+
+    //   self.setState({alerts: alerts});
+    //   self.setState({lastFetched: Date.now()});
+    //   self.setState({timeSinceFetched: moment(Date.now()).fromNow()});
+    // });
+  }
+
+  fetchRSS(url) {
     var parser = require('rss-parser');
     let self = this;
 
@@ -105,6 +138,8 @@ class Alerts extends Component {
       self.setState({alerts: alerts});
       self.setState({lastFetched: Date.now()});
       self.setState({timeSinceFetched: moment(Date.now()).fromNow()});
+
+      return;
     });
   }
 
@@ -113,15 +148,20 @@ class Alerts extends Component {
       clearInterval(this.state.intervalId);
   }
 
+  update() {
+    this.setState({alerts: null});
+    this.fetchRSS(this.state.rssFeed);
+  }
+
   render() {
-    
     if (this.state.alerts) {
       return(
         <div id='alerts'>
           
           <AlertHeader
             alertCount={ this.state.alerts.length }
-            updatedAt={ this.state.timeSinceFetched } />
+            updatedAt={ this.state.timeSinceFetched }
+            handler={ this.update } />
 
           <ul>
             {this.state.alerts.map(function(a, i){
